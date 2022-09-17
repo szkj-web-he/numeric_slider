4677a4a6a4f070319e5b588a219ff9e7793e2e72
szkj-web-he <xuejie.he@datareachable.com>
2022-09-17 17:29:49 +0800

    1

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import "./style.scss";
 import { ConfigYML, PluginComms } from "@possie-engine/dr-plugin-sdk";
 import Header from "./header";
 import MainContent from "./main";
+import { ScrollComponent } from "./Scroll";
 
 export const comms = new PluginComms({
     defaultConfig: new ConfigYML(),
@@ -35,8 +36,10 @@ const Main: React.FC = () => {
     /* <------------------------------------ **** FUNCTION END **** ------------------------------------ */
     return (
         <div className="wrapper">
-            <Header />
-            <MainContent />
+            <ScrollComponent>
+                <Header />
+                <MainContent />
+            </ScrollComponent>
         </div>
     );
 };
